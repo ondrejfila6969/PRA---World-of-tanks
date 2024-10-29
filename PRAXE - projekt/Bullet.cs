6f7b58f1a53e2fce7622b71c03374866414a76bd
Ondřej Fíla <114986357+ondrejfila6969@users.noreply.github.com>
2024-10-29 09:51:30 +0100

    Part 2 completed

---
+++ PRAXE - projekt/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PRAXE___projekt
+{
+    // Třída Bullet pro ukládání pozice a směru každého projektilu
+    public class Bullet
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Direction { get; }
+        public float Rotation { get; }
+        public Bullet(Vector2 position, Vector2 direction, float rotation)
+        {
+            Position = position;
+            Direction = direction;
+            Rotation = rotation;
+        }
+    }
+}
--- PRAXE - projekt/Game1.cs
+++ PRAXE - projekt/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace PRAXE___projekt
 {
@@ -9,9 +11,18 @@ namespace PRAXE___projekt
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        private Texture2D _tankTexture; // 2D textura pro tank
         private Vector2 _tankPosition;
-        private float _tankSpeed = 100f; // Rychlost tanku
+        private float _tankSpeed = 100f;
+
+        private Texture2D _tankBodyTexture;
+        private Texture2D _tankBarrelTexture;
+        private Texture2D _bulletTexture;
+
+        private float _rotationAngle; // Úhel natočení hlavně podle myši
+        private List<Bullet> _bullets = new List<Bullet>();
+        private float _bulletSpeed = 200f;
+        private int _bulletWidth = 8;
+        private int _bulletHeight = 4;
 
         public Game1()
         {
@@ -22,32 +33,75 @@ namespace PRAXE___projekt
 
         protected override void Initialize()
         {
-            _tankPosition = new Vector2(100, 100); // Počáteční pozice tanku
+            _tankPosition = new Vector2(200, 200); // Počáteční souřadnice tanku
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _tankTexture = Content.Load<Texture2D>("tank"); // Načte texturu tanku
+
+            // Textura těla tanku
+            _tankBodyTexture = new Texture2D(GraphicsDevice, 60, 40);
+            Color[] bodyData = new Color[60 * 40];
+            for (int i = 0; i < bodyData.Length; i++) bodyData[i] = Color.DarkGreen;
+            _tankBodyTexture.SetData(bodyData);
+
+            // Textura hlavně tanku
+            _tankBarrelTexture = new Texture2D(GraphicsDevice, 40, 10);
+            Color[] barrelData = new Color[40 * 10];
+            for (int i = 0; i < barrelData.Length; i++) barrelData[i] = Color.Gray;
+            _tankBarrelTexture.SetData(barrelData);
+
+            // Textura projektilu
+            _bulletTexture = new Texture2D(GraphicsDevice, _bulletWidth, _bulletHeight);
+            Color[] bulletData = new Color[_bulletWidth * _bulletHeight];
+            for (int i = 0; i < bulletData.Length; i++) bulletData[i] = Color.Black;
+            _bulletTexture.SetData(bulletData);
         }
 
         protected override void Update(GameTime gameTime)
         {
             var keyboardState = Keyboard.GetState();
+            var mouseState = Mouse.GetState();
 
-            // Pohyb pomocí W, A, S, D kláves
-            if (keyboardState.IsKeyDown(Keys.W))
+            // Pohyb W, A, S, D + kontrola hranic
+            if (keyboardState.IsKeyDown(Keys.W) && _tankPosition.Y > 0)
                 _tankPosition.Y -= _tankSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (keyboardState.IsKeyDown(Keys.S))
+            if (keyboardState.IsKeyDown(Keys.S) && _tankPosition.Y < GraphicsDevice.Viewport.Height - _tankBodyTexture.Height)
                 _tankPosition.Y += _tankSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (keyboardState.IsKeyDown(Keys.A))
+            if (keyboardState.IsKeyDown(Keys.A) && _tankPosition.X > 0)
                 _tankPosition.X -= _tankSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (keyboardState.IsKeyDown(Keys.D))
+            if (keyboardState.IsKeyDown(Keys.D) && _tankPosition.X < GraphicsDevice.Viewport.Width - _tankBodyTexture.Width)
                 _tankPosition.X += _tankSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Výpočet úhlu natočení hlavně podle směru k myši
+            Vector2 direction = new Vector2(mouseState.X, mouseState.Y) - (_tankPosition + new Vector2(_tankBodyTexture.Width / 2, _tankBodyTexture.Height / 2));
+            _rotationAngle = (float)Math.Atan2(direction.Y, direction.X);
+
+            // Střelba, když uživatel klikne levým tlačítkem myši
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                // Výpočet konce hlavně
+                Vector2 barrelEnd = _tankPosition + new Vector2(
+                    (float)Math.Cos(_rotationAngle) * (_tankBodyTexture.Width / 2 + _tankBarrelTexture.Width / 2),
+                    (float)Math.Sin(_rotationAngle) * (_tankBodyTexture.Width / 2 + _tankBarrelTexture.Width / 2)
+                );
+
+                Vector2 bulletDirection = Vector2.Normalize(new Vector2(mouseState.X, mouseState.Y) - barrelEnd);
+
+                // Přidání nového projektilu do listu
+                _bullets.Add(new Bullet(barrelEnd, bulletDirection, _rotationAngle));
+            }
+
+            // Aktualizace pozice všech projektilů
+            for (int i = 0; i < _bullets.Count; i++)
+            {
+                _bullets[i].Position += _bullets[i].Direction * _bulletSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
 
             base.Update(gameTime);
         }
@@ -57,8 +111,32 @@ namespace PRAXE___projekt
             GraphicsDevice.Clear(Color.White);
 
             _spriteBatch.Begin();
-            // Vykreslení tanku
-            _spriteBatch.Draw(_tankTexture, _tankPosition, null, Color.White, 0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0f);
+
+            // Vykreslení těla tanku
+            _spriteBatch.Draw(_tankBodyTexture, _tankPosition, Color.White);
+
+            // Vykreslení hlavně tanku s rotací
+            Vector2 barrelOrigin = new Vector2(0, _tankBarrelTexture.Height / 2);
+            Vector2 barrelPosition = _tankPosition + new Vector2(_tankBodyTexture.Width / 2, _tankBodyTexture.Height / 2);
+
+            _spriteBatch.Draw(
+                _tankBarrelTexture,
+                barrelPosition,
+                null,
+                Color.White,
+                _rotationAngle,
+                barrelOrigin,
+                1.0f,
+                SpriteEffects.None,
+                0f
+            );
+
+            // Vykreslení všech projektilů
+            foreach (var bullet in _bullets)
+            {
+                _spriteBatch.Draw(_bulletTexture, bullet.Position, null, Color.White, bullet.Rotation, new Vector2(0, _bulletHeight / 2), 1.0f, SpriteEffects.None, 0f);
+            }
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
